6d3d9243d4f80be676d0ab897fed3034312eaff4
caype <chayakrishnaprasad@gmail.com>
2016-11-17 15:39:27 +0000

    removed unnecessary api calls

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var port = process.env.PORT || 8080;
 var app = express();
 const mongoDBUrl = process.env.MONGOLAB_URI;
 
-app.get('/recentSearches/all',function(req,res) {
+app.get('/recentSearches',function(req,res) {
    mongoClient.connect(mongoDBUrl,function(err,db){
        if(err) throw err;
        var recentSearchTable = db.collection('imageAbstractionRecentSearches');
-       recentSearchTable.find({}).toArray(function(err,searchResults){
+       recentSearchTable.find().sort({_id:1}).toArray(function(err,searchResults){
            if(err) throw err;
 
            if(searchResults.length>0)
@@ -20,43 +20,59 @@ app.get('/recentSearches/all',function(req,res) {
    }) ;
 });
 
-app.get('/:queryParam?/:offset?',function(req,res){
+app.get('/delete',function(req,res){
+       mongoClient.connect(mongoDBUrl,function(err,db){
+           if(err) throw err;
+        var tableToRemove = db.collection('imageAbstractionRecentSearches');
+        tableToRemove.remove({},function(err,resu) {
+                if(err) throw err;
+                res.send(resu);
+            });
+    }); 
+});
+
+app.get('/:queryParam*?/:offset*?',function(req,res){
     
     var targetData=[];
-    var resultantData=[];
-    var dateObj = new Date();
-    var datetimeNow = dateObj.getDate()+'/'+dateObj.getMonth()+1+'/'+dateObj.getFullYear()+' '+dateObj.getHours()+":"+dateObj.getMinutes()+":"+dateObj.getSeconds();
+    var dateObj = new Date().getTime();
     
     if (req.params.queryParam!= undefined || req.params.queryParam != null) {
-        var jpgUrl = {
-            url:'https://api.imgur.com/3/gallery/search/top/?q='+req.params.queryParam,
+        var RequestUrl = {
+            url:'https://api.imgur.com/3/gallery/search/top?q='+req.params.queryParam,
             headers:{
                 "Authorization":"Client-ID "+process.env.client_id
             }
         };
         
-        request(jpgUrl,function(error,response,body){
+        request(RequestUrl,function(error,response,body){
             if(error) throw error;
             
             if (!error && response.statusCode == 200) {
-                var imginfo = JSON.parse(body);
+                var info = JSON.parse(body);
 
-                for (var i = 0; i <  imginfo.data.length; i++) {
-                    if( imginfo.data[i].link.indexOf('.jpg')!= -1 || imginfo.data[i].link.indexOf('.png')!= -1 || imginfo.data[i].link.indexOf('.gif')!= -1)
-                    targetData.push({title: imginfo.data[i].title,link: imginfo.data[i].link,size:imginfo.data[i].size,type:imginfo.data[i].type});
+                for (var i = 0; i < info.data.length; i++) {
+                    if(info.data[i].link.indexOf('.jpg')!= -1 || info.data[i].link.indexOf('.png')!= -1 || info.data[i].link.indexOf('.gif')!= -1)
+                    targetData.push({title:info.data[i].title,link:info.data[i].link});
                 }
-                
+                    /*
+                    if(req.params.offset!= undefined || req.params.offset!=null){
+                        if(targetData.length>=10*req.params.offset){
+                            var fval = req.params.offset==1?0:req.params.offset-1;
+                            targetData=targetData.slice(10*fval,10*req.params.offset);    
+                        }
+                    }
+                    */
+                    
                 mongoClient.connect(mongoDBUrl,function(err,db){
                     if(err) throw err;
                     var targetTable = db.collection('imageAbstractionRecentSearches');
-                    if(req.params.queryParam !='favicon.ico'){
-                        var recordToInsert={searchTerm:req.params.queryParam,timestamp:datetimeNow};
-                    targetTable.insert([recordToInsert],function(err,insertedRecord){
-                        if(err) throw err;
-                        
-                        res.send(targetData);
-                    });
-                    } 
+                    if (req.params.queryParam != 'favicon.ico') {
+                        var recordToInsert={searchTerm:req.params.queryParam,timestamp:dateObj};
+                        targetTable.insert([recordToInsert],function(err,insertedRecord){
+                            if(err) throw err;
+                            res.send(targetData);
+                        });   
+                    }
                 });
               }
         });
@@ -67,8 +83,6 @@ app.get('/:queryParam?/:offset?',function(req,res){
 });
 
 
-
 app.listen(port,function(){
     console.log("something is happening at https://aqueous-chamber-90195.herokuapp.com ");
 });
-
